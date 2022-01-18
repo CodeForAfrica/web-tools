752f8a999789c10f56235fbfd078e0a3cbe0d844
esir <esirkings@gmail.com>
2022-01-18 16:55:52 +0300

    Adds new report for 'Kenya Disinformation Risk Assessment Report Online'

--- src/components/tools/media/ResourceList.js
+++ src/components/tools/media/ResourceList.js
@@ -11,11 +11,14 @@ const localMessages = {
   saElectionsTitle: { id: 'marketing.globalCollections.title', defaultMessage: 'A race to the bottom' },
   saElectionsDescription: { id: 'marketing.globalCollections.description', defaultMessage: `<h3>Populism, mis/disinformation and South Africa’s 2021 election</h3><p>An overview and analysis of the roles populism and mis/disinformation played in the run-up to South Africa’s 2021 Local Government Elections.<br/>Produced by the South African Anti-Disinformation Project.<br/> <a href="${assetUrl('/static/docs/A race to the bottom _ SA Elections 2021 _Disinfo Project.pdf')}" target = "_blank">Download the report</a> </p>` },
 
-attentionTitle: { id: 'marketing.attention.title', defaultMessage: 'GDI Risk Assessment: South African Media Market' },
-attentionDescription: { id: 'marketing.attention.description', defaultMessage: '<p>The Global Disinformation Index 2019 provides an in-depth breakdown of the disinformation risks recorded across the South African media market, based on findings from 30 of some of the top news sites in the country. (2020 report launches January 2021) <br/> <a href="https://disinformationindex.org/wp-content/uploads/2020/01/SA-Risk-Assessment-Report-Screen-26-Jan.pdf">Download the full DNR 2020 report</a> </p>' },
+  onlineNewsMarketKenya: { id: 'marketing.attention.title', defaultMessage: 'Disinformation Risk Assessment' },
+  onlineNewsMarketKenyaDescription: { id: 'marketing.attention.description', defaultMessage: `<h3>The Online News Market in Kenya</h3><p>The 2021 Global Disinformation Index report has released a disinformation risk index for some of the most popular news sites in Kenya based on a comprehensive assessment framework.<br/><a href="${assetUrl('/static/docs/Kenya Disinformation Risk Assessment Report Online.pdf')}" target = "_blank">Download the report</a></p>` },
 
-gdiMediaMarketRiskTitle: { id: 'marketing.attention.title', defaultMessage: 'GDI Media Market Risk Assessment: South Africa 2020' },
-gdiMediaMarketDescription: { id: 'marketing.attention.description', defaultMessage: `<p>The Global Disinformation Index and Code for Africa has produced the annual overview of disinformation risk ratings for some of the most visited media sites in South Africa. <br/> <a href="${assetUrl('/static/docs/South Africa Risk Ratings Report.pdf')}" target = "_blank">Download the report</a> </p>` },
+  attentionTitle: { id: 'marketing.attention.title', defaultMessage: 'GDI Risk Assessment: South African Media Market' },
+  attentionDescription: { id: 'marketing.attention.description', defaultMessage: '<p>The Global Disinformation Index 2019 provides an in-depth breakdown of the disinformation risks recorded across the South African media market, based on findings from 30 of some of the top news sites in the country. (2020 report launches January 2021) <br/> <a href="https://disinformationindex.org/wp-content/uploads/2020/01/SA-Risk-Assessment-Report-Screen-26-Jan.pdf">Download the full DNR 2020 report</a> </p>' },
+
+  gdiMediaMarketRiskTitle: { id: 'marketing.attention.title', defaultMessage: 'GDI Media Market Risk Assessment: South Africa 2020' },
+  gdiMediaMarketDescription: { id: 'marketing.attention.description', defaultMessage: `<p>The Global Disinformation Index and Code for Africa has produced the annual overview of disinformation risk ratings for some of the most visited media sites in South Africa. <br/> <a href="${assetUrl('/static/docs/South Africa Risk Ratings Report.pdf')}" target = "_blank">Download the report</a> </p>` },
 };
 
 
@@ -25,6 +28,11 @@ const ExplorerMarketingFeatureList = () => (
       titleMsg={localMessages.saElectionsTitle}
       contentMsg={localMessages.saElectionsDescription}
       imageName="A-race-to-the-bottom-_-SA-Elections-2021-_Disinfo-Project-1.jpg"
+    />
+    <ResourceFeatureItem
+      titleMsg={localMessages.onlineNewsMarketKenya}
+      contentMsg={localMessages.onlineNewsMarketKenyaDescription}
+      imageName="Kenya-Disinformation-Risk-Assessment-Report-Online-1.jpg"
       imageOnLeft
     />
     <ResourceFeatureItem
