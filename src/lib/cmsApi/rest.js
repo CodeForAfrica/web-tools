bb4abb9396d90c037088f20172c3ac787dc8912c
Michael Hudson Nkotagu <mnkotagu@gmail.com>
2024-10-03 14:07:23 +0300

    chore: Implement CMS rest API

---
+++ src/lib/cmsApi/rest.js
@@ -0,0 +1,16 @@
+import { createApiPromise } from '../apiUtil';
+
+const BASE_URL = process.env.PAYLOAD_API_URL;
+
+export function getPageContent(applicationName, pageName) {
+  return createApiPromise(
+    `${BASE_URL}/${applicationName}-pages`, { '[slug][equals]': pageName }
+  );
+}
+
+
+export function getCollections(collectionName) {
+  return createApiPromise(
+    `${BASE_URL}/${collectionName}`
+  );
+}
