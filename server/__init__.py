f6ffa40f7757ced5a1f15c45bd31f644a20d4d0b
Michael Hudson Nkotagu <m453h@users.noreply.github.com>
2024-12-12 13:18:31 +0300

    Update server/__init__.py
    Co-authored-by: Clemence Kyara <kilemensi@users.noreply.github.com>

--- server/__init__.py
+++ server/__init__.py
@@ -242,6 +242,7 @@ import server.views.sources.search
 import server.views.metadata
 import server.views.platforms
 import server.views.cms.cms
+
 if (server_app == SERVER_APP_SOURCES) or is_dev_mode():
     import server.views.sources.collection
     import server.views.sources.collectionedit
